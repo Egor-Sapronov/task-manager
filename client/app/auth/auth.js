d4ca779cfc4838491ede33fa7bf59bdcc130c0fb
Egor <sapronov.egor@gmail.com>
2014-12-14 13:01:59 +0600

    login

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,7 +1,38 @@
 module.exports = (function () {
     angular.module('components.auth', [])
         .factory('userService', userService)
-        .factory('authenticationService', authenticationService);
+        .factory('authenticationService', authenticationService)
+        .controller('AccountController', AccountController)
+        .controller('LoginController', LoginController);
+
+    AccountController.$inject = ['userService'];
+    function AccountController(userService) {
+        var vm = this;
+
+        vm.username = '';
+
+        userService.userInfo().success(function (data) {
+            vm.username = data.name;
+        });
+    }
+
+    LoginController.$inject = ['$window', '$location', 'userService', 'authenticationService'];
+    function LoginController($window, $location, userService, authenticationService) {
+        var vm = this;
+        vm.login = function (username, password) {
+            userService.logIn(username, password)
+                .success(function (data) {
+                    authenticationService.isLogged = true;
+                    authenticationService.username = username;
+                    $window.sessionStorage.Bearer = data.access_token;
+                    $window.sessionStorage.User = username;
+                    $location.path('/');
+                })
+                .error(function (status, data) {
+
+                });
+        };
+    }
 
     userService.$inject = ['$http', '$window'];
     function userService($http, $window) {
--- client/app/init.js
+++ client/app/init.js
@@ -4,7 +4,33 @@
         'ngRoute',
         'components.auth'
     ])
-        .config(['$routeProvider', router]);
+        .config(['$routeProvider', router])
+        .factory('TokenInterceptor', function ($window) {
+            return {
+                request: function (config) {
+                    if ($window.sessionStorage.Bearer) {
+                        config.headers['Authorization'] = 'Bearer ' + $window.sessionStorage.Bearer;
+                    }
+                    return config;
+                }
+            };
+        })
+        .factory('AccessInterceptor', function ($q, $location) {
+            return {
+                'responseError': function (rejection) {
+                    console.log(rejection.status);
+
+                    if (rejection.status === 401)
+                        $location.path('/login');
+
+                    return $q.reject(rejection);
+                }
+            };
+        })
+        .config(function ($httpProvider) {
+            $httpProvider.interceptors.push('TokenInterceptor');
+            $httpProvider.interceptors.push('AccessInterceptor');
+        });
 
     function router($routeProvider) {
         $routeProvider
