e06d9873c3d2d2444549511d01cc290d0735c738
Egor <sapronov.egor@gmail.com>
2014-12-14 12:39:47 +0600

    auth templates

--- client/app/init.js
+++ client/app/init.js
@@ -1,3 +1,33 @@
 (function () {
-    angular.module('app', []);
+    require('./auth/auth.js');
+    angular.module('app', [
+        'ngRoute',
+        'components.auth'
+    ])
+        .config(['$routeProvider', router]);
+
+    function router($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/templates/index.html'
+            })
+            .when('/tasks', {
+                templateUrl: '/templates/tasks.html'
+            })
+            .when('/createtask', {
+                templateUrl: '/templates/createtask.html'
+            })
+            .when('/account', {
+                templateUrl: '/templates/auth/account.html'
+            })
+            .when('/login', {
+                templateUrl: '/templates/auth/login.html'
+            })
+            .when('/register', {
+                templateUrl: '/templates/auth/register.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
 })();
